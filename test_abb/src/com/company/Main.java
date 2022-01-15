72b9293585f2ebf2e14c2e2aacc4b0b0f7538d81
AZIZGASIMOV94 <gasimovaziz@gmail.com>
2022-01-16 00:49:08 +0400

    project added to the repo

---
+++ test_abb/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int[] cars = {6, 2, 12, 7};
+        int k = 3;
+        System.out.println(carParkingRoof(cars,k));
+
+    }
+
+    public static int carParkingRoof(int[] carSpots,int kMinLen){
+        Arrays.sort(carSpots);
+        //[2,6,7,12]
+        List<Integer> ls = new ArrayList<>();
+        List<Integer> lenLs = new ArrayList<>();
+        int len = 0;
+
+        for(int i = 0; i < carSpots.length; i++){
+            ls.add(carSpots[i]);
+            if(ls.size() == kMinLen){
+                len = ls.get(2) - ls.get(0) + 1;
+                lenLs.add(len);
+                len = 0;
+                ls.removeAll(ls);
+                i = i-2;
+            }
+        }
+        Collections.sort(lenLs);
+
+        return lenLs.get(0);
+    }
+}
